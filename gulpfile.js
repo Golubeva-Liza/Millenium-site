064fab155de2177de63ea3b27fb5dcbb4338341f
LizaGolubeva <sp6lisa@yandex.ru>
2021-10-14 13:00:21 +0300

    Начало верстки нового проекта: подготовка

---
+++ gulpfile.js
@@ -0,0 +1,128 @@
+"use strict";
+
+const gulp = require("gulp");
+const webpack = require("webpack-stream");
+const browserSync = require("browser-sync");
+const sass = require('gulp-sass')(require('sass'));
+const cleanCSS = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
+const rename = require("gulp-rename");
+const htmlmin = require('gulp-htmlmin');
+
+const dist = "./dist/";
+
+gulp.task('html', function () {
+  return gulp.src("src/*.html")
+      .pipe(htmlmin({ collapseWhitespace: true }))
+      .pipe(gulp.dest("dist/"));
+});
+
+gulp.task('styles', function() {
+  return gulp.src("src/sass/**/*.+(scss|sass)")
+      .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+      .pipe(rename({suffix: '.min', prefix: ''}))
+      .pipe(autoprefixer())
+      .pipe(cleanCSS({compatibility: 'ie8'}))
+      .pipe(gulp.dest("dist/css"))
+      .pipe(browserSync.stream());
+});
+
+// gulp.task('fonts', function () {
+//   return gulp.src("src/fonts/**/*")
+//       .pipe(gulp.dest("dist/fonts"))
+//       .pipe(browserSync.stream());
+// });
+
+// gulp.task('icons', function () {
+//   return gulp.src("src/icons/**/*")
+//       .pipe(gulp.dest("dist/icons"))
+//       .pipe(browserSync.stream());
+// });
+
+// gulp.task('images', function () {
+//   return gulp.src("src/img/**/*")
+//       .pipe(gulp.dest("dist/img"))
+//       .pipe(browserSync.stream());
+// });
+
+gulp.task("copy-assets", () => {
+  return gulp.src("./src/assets/**/*.*")
+    .pipe(gulp.dest(dist + "/assets"))
+    .on("end", browserSync.reload);
+});
+
+gulp.task("build-js", () => {
+    return gulp.src("./src/js/main.js")
+      .pipe(webpack({
+          mode: 'development',
+          output: {
+              filename: 'script.js'
+          },
+          watch: false,
+          devtool: "source-map",
+          module: {
+              rules: [
+                {
+                  test: /\.m?js$/,
+                  exclude: /(node_modules|bower_components)/,
+                  use: {
+                    loader: 'babel-loader',
+                    options: {
+                      presets: [['@babel/preset-env', {
+                          debug: true,
+                          corejs: 3,
+                          useBuiltIns: "usage"
+                      }]]
+                    }
+                  }
+                }
+              ]
+            }
+      }))
+      .pipe(gulp.dest(dist))
+      .on("end", browserSync.reload);
+});
+
+gulp.task("watch", () => {
+    browserSync.init({
+		server: "./dist/",
+		port: 4000,
+		notify: true
+    });
+    
+    gulp.watch("src/*.html").on('change', gulp.parallel('html'));
+    gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel('styles'));
+    gulp.watch("./src/assets/**/*.*", gulp.parallel("copy-assets"));
+
+    gulp.watch("./src/js/**/*.js", gulp.parallel("build-js"));
+});
+
+gulp.task("build-prod-js", () => {
+    return gulp.src("./src/js/main.js")
+      .pipe(webpack({
+          mode: 'production',
+          output: {
+              filename: 'script.js'
+          },
+          module: {
+              rules: [
+                {
+                  test: /\.m?js$/,
+                  exclude: /(node_modules|bower_components)/,
+                  use: {
+                    loader: 'babel-loader',
+                    options: {
+                      presets: [['@babel/preset-env', {
+                          corejs: 3,
+                          useBuiltIns: "usage"
+                      }]]
+                    }
+                  }
+                }
+              ]
+            }
+      }))
+      .pipe(gulp.dest(dist));
+});
+
+gulp.task('default', gulp.parallel('watch', 'copy-assets', 'styles', 'html', "build-js"));
